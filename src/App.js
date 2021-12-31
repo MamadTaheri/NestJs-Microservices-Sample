697294803ac4bcfbebaec4feacfad53f373a500b
MamadTaheri68 <mamad.taheri.68@gmail.com>
2021-12-31 21:30:03 +0330

    add login to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <h2>let's build the linkedinn clone ❤❤</h2>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+         </Routes>
+        </Router>
     </div>
   );
 }
