30a830c8260d09b8c5f6480c5bb79b510bc55ad8
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-01-02 23:14:19 +0330

    update RightSideComponent

--- src/components/home/RightSideComponent.jsx
+++ src/components/home/RightSideComponent.jsx
@@ -2,12 +2,51 @@ import styled from "styled-components";
 
 const RightSideComponent = (props) => {
     return (
-        <Container>Right side</Container>
+        <Container>
+            <FollowCard>
+                <Title>
+                    <h2>Add to your feed</h2>
+                    <img src="/images/feed-icon.svg" alt="feed-icon" />
+                </Title>
+                <FeedList>
+                    <li>
+                        <a>
+                            <Avatar />
+                        </a>
+                        <div>
+                            <span>#Linkedin</span>
+                            <button>Follow</button>
+                        </div>
+                    </li>
+                </FeedList>
+            </FollowCard>
+        </Container>
     );
 }
 
 const Container = styled.div`
     grid-area: Rightside;
+`;
+
+const FollowCard = styled.div`
+    text-align: center;
+    overflow: hidden;
+    margin-bottom: 8px;
+    background-color: #fff;
+    border-radius: 5px;
+    position: relative;
+    border: none;
+    box-shadow: 0 0 0 1px rgb(0 0 0 / 15%), 0 0 0 rgb(0 0 0 / 20%);
+    padding: 12px;
+`;
+
+const Title = styled.div`
+    display: inline-flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    width: 100%;
+    color: rgba(0,0,0,0.6);
 `;
 
 export default RightSideComponent;
