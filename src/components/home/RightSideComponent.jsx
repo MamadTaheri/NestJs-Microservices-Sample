ba2bd318fd16441b4cf3983f9fcd9ae2118a6b39
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-01-03 00:32:13 +0330

    update RightSideComponent

--- src/components/home/RightSideComponent.jsx
+++ src/components/home/RightSideComponent.jsx
@@ -18,6 +18,15 @@ const RightSideComponent = (props) => {
                             <button>Follow</button>
                         </div>
                     </li>
+                    <li>
+                        <a>
+                            <Avatar />
+                        </a>
+                        <div>
+                            <span>#Video</span>
+                            <button>Follow</button>
+                        </div>
+                    </li>
                 </FeedList>
             </FollowCard>
         </Container>
@@ -47,6 +56,47 @@ const Title = styled.div`
     font-size: 16px;
     width: 100%;
     color: rgba(0,0,0,0.6);
+`;
+
+const FeedList = styled.ul`
+    margin-top: 16px;
+    li {
+        display: flex;
+        align-items: center;
+        margin: 12px 0;
+        position: relative;
+        font-size: 14px;
+        & > div {
+            display: flex;
+            flex-direction: column;
+        }
+        button {
+            background-color: transparent;
+            color: rgba(0,0,0,0.6);
+            box-shadow: inset 0 0 0 1px rgba(0,0,0,0.6);
+            padding: 11px 16px;
+            align-items: center;
+            border-radius: 15px;
+            box-sizing: border-box;
+            font-weight: 600;
+            display: inline-flex;
+            justify-content: center;
+            max-height: 32px;
+            max-width: 480px;
+            text-align: center;
+            outline: none;
+        }
+    }
+`;
+
+const Avatar = styled.div`
+    background-image: url('images/Hashtag.png');
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+    width: 48px;
+    height: 48px;
+    margin-right: 8px;
 `;
 
 export default RightSideComponent;
