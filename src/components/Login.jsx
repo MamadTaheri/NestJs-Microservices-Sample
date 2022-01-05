f196abfcafeca84d06b26045c7fa7370657f87c2
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-01-06 01:16:46 +0330

    implement signIn with firebase

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import {signInApi} from "../actions";
+import {Navigate } from "react-router";
 
 const Login = (props) => {
+
     return (
         <Container>
+            {
+                props.user && <Navigate  to="/home" />
+            }
             <Nav>
                 <a href="/">
                     <img src="/images/login-logo.svg" alt="login-logo" />
@@ -168,7 +173,9 @@ const Google = styled.button`
 `;
 
 const mapStateToProps = (state) => {
-    return {};
+    return {
+        user: state.userState.user
+    };
 };
 
 const mapDispatchToProps = (dispatch) => ({
