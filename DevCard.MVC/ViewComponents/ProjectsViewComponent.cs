8769ecc73c11cb5dd0edf6ecad278e5d80955bbf
MamadTaheri68 <mamad.taheri.68@gmail.com>
2022-06-02 11:03:18 +0430

    update

---
+++ DevCard.MVC/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DevCard.MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard.MVC.ViewComponents
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var projects = new List<Project>
+            {
+                new Project(1,"تاکسی", "درخواست آنلاین تاکسی برای سفرهای درون شهری", "project-1.jpg", "Snapp"),
+                new Project(2, "زودفود", "درخواست آنلاین غذا", "project-2.jpg", "Snapp food"),
+                new Project(3, "مدارس", "سیستم مدیریت یکپارچه مدارس", "project-3.jpg", "Schoole"),
+                new Project(4, "فضاپیما", "برنامه مدیریت فضاپیما های ناسا", "project-4.jpg", "NASA"),
+            };
+            return View("_Projects", projects);
+        }
+    }
+}
