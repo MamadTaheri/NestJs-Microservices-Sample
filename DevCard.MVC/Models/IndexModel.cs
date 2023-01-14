de753739130fe87712d9de77c59bef82b455307e
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-01-14 16:45:49 +0330

    refresh

--- DevCard.MVC/Models/IndexModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DevCard.MVC.Models
-{
-    public class IndexModel
-    {
-        public List<Project> Projects { get; set; }
-        public List<Article> Articles { get; set; }
-    }
-}
