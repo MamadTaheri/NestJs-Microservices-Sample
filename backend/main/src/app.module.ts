b382d50840ba776c84c9f3110bb813013fb05394
MamadTaheri68 <mamad.taheri.68@gmail.com>
2023-01-15 04:46:04 +0330

    add product in main micro-service

--- backend/main/src/app.module.ts
+++ backend/main/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/Nest_MainDB', {
       autoCreate: true,
     }),
+    ProductModule,
   ],
   controllers: [AppController],
   providers: [AppService],
